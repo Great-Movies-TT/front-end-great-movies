8260be64266f7fad74f3cb1c826b7d28f04794d8
Mykola Maik <mykoluamaik@gmail.com>
2024-09-24 14:27:54 +0300

    added routing

---
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -0,0 +1,5 @@
+function FavoritesPage() {
+  return <h1>This is FavoritesPage</h1>;
+}
+
+export default FavoritesPage;
---
+++ src/pages/MovieDetailsPage/MovieDetailsPage.tsx
@@ -0,0 +1,5 @@
+function MovieDetailsPage() {
+  return <h1>This is MovieDetailsPage</h1>;
+}
+
+export default MovieDetailsPage;
