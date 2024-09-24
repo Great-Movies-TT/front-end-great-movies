d65e709eb4aef2fd2a11b05b5653d80af30a328f
Mykola Maik <mykoluamaik@gmail.com>
2024-09-24 15:47:31 +0300

    setup-mui-theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import RouterProvider from "./containers/providers/routerProvider/RouterProvider";
 import { Provider } from "react-redux";
+import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
 import store from "./redux/store";
+import muiTheme from "./styles/muiTheme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider />
+      <MuiThemeProvider theme={muiTheme}>
+        <RouterProvider />
+      </MuiThemeProvider>
     </Provider>
   </StrictMode>
 );
